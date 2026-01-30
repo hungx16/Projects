adb90d4700c63d1112bf863377002c610a43906e
hung16 <103294332+hungx16@users.noreply.github.com>
2026-01-30 20:00:38 +0700

    Add files via upload

---
+++ student_list.Application/Application.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using student_list.Domain.Entities;
+using student_list.Domain.Models;
+
+namespace student_list.Application
+{
+    public class Application
+    {
+
+    }
+
+    public class StudentProfile : Profile
+    {
+        public StudentProfile()
+        {
+            CreateMap<Student, StudentModel>().ReverseMap();
+        }
+    }
+}
